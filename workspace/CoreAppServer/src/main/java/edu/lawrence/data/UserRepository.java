ae24e9b80b81661b2b6fe6efd8d03b50c40ae10e
Malik Graham <malikgraham15@yahoo.com>
2016-02-19 22:17:35 -0600

    Added a query to the repository that returns a User

--- workspace/CoreAppServer/src/main/java/edu/lawrence/data/UserRepository.java
+++ workspace/CoreAppServer/src/main/java/edu/lawrence/data/UserRepository.java
@@ -1,8 +1,6 @@
 package edu.lawrence.data;
 
 
-import java.util.Collection;
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -15,5 +13,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	User findByUsername(String username);
 	User findByPassword(String password);
 	String findUsernameByUsernameAndPassword(String Username, String Password);
-	//String findIdByUsernameAndPassword()
+	User findByUsernameAndPassword(String Username, String Password);
 }
--- workspace/CoreAppServer/src/main/java/edu/lawrence/rest/UserRestController.java
+++ workspace/CoreAppServer/src/main/java/edu/lawrence/rest/UserRestController.java
@@ -27,11 +27,11 @@ public class UserRestController {
 	
 	@ResponseBody
 	@RequestMapping("/user")
-	public String validateUser(@RequestParam Map<String, String> userInfo) {
+	public User validateUser(@RequestParam Map<String, String> userInfo) {
 		String[] username = (userInfo.keySet()).toArray(new String[0]);
 		String[] password = (userInfo.values()).toArray(new String[0]);
-		if (this.userRepository.findUsernameByUsernameAndPassword(username[0], password[0]) != null) {
-			return this.userRepository.findUsernameByUsernameAndPassword(username[0], password[0]);
+		if (this.userRepository.findByUsernameAndPassword(username[0], password[0]) != null) {
+			return this.userRepository.findByUsernameAndPassword(username[0], password[0]);
 		}
 	return null;
 	}
