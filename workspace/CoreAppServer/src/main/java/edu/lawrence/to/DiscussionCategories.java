1b330ebd786ab71b38a7c612b0494ce185aa9f99
Malik Graham <malikgraham15@yahoo.com>
2016-03-24 16:48:08 -0400

    Added the entity class: DiscussionCategories

---
+++ workspace/CoreAppServer/src/main/java/edu/lawrence/to/DiscussionCategories.java
@@ -0,0 +1,46 @@
+package edu.lawrence.to;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import org.hibernate.annotations.GenericGenerator;
+import java.util.Date;
+
+/**
+ * @author Malik Graham
+ */
+@Entity
+@Table(name = "DiscussionCategories")
+public class DiscussionCategories {
+	@Id
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	@GeneratedValue(generator = "uuid")
+	@Column(name = "discussioncategoryid")
+	private String discussioncategoryid;
+	@Column(name = "category")
+	private String category;
+	@Column(name = "date")
+	private Date date;
+	
+	public String getDiscussioncategoryid() {
+		return discussioncategoryid;
+	}
+	public void setDiscussioncategoryid(String discussioncategoryid) {
+		this.discussioncategoryid = discussioncategoryid;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+}
