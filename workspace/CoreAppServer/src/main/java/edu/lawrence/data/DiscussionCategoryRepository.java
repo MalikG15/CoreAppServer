6a5fd6a4ed72cb335ba49eb72c85b08ecfcb9a4b
Malik Graham <malikgraham15@yahoo.com>
2016-03-25 16:45:41 -0400

    Committing the DiscussionCategoryRepository

---
+++ workspace/CoreAppServer/src/main/java/edu/lawrence/data/DiscussionCategoryRepository.java
@@ -0,0 +1,18 @@
+package edu.lawrence.data;
+
+import edu.lawrence.to.DiscussionCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Date;
+
+/**
+ * @author Malik Graham
+ */
+public interface DiscussionCategoryRepository extends JpaRepository<DiscussionCategory, Long> {
+	List<DiscussionCategory> findByCategory(String category);
+	List<DiscussionCategory> findAllOrderByDateDesc();
+	List<DiscussionCategory> findAllOrderByCategoryDesc();
+	List<DiscussionCategory> findByDateGreaterThanEqual(Date date);
+	List<DiscussionCategory> findByDateLessThanEqual(Date date);
+	List<DiscussionCategory> findAll();
+}
