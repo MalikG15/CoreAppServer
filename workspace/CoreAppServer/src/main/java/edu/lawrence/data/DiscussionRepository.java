67145d7ef962459285a7ca7b0708f72a5a81c1ca
Malik Graham <malikgraham15@yahoo.com>
2016-03-25 17:15:11 -0400

    Added more queries for specific discussions in discussion categories

---
+++ workspace/CoreAppServer/src/main/java/edu/lawrence/data/DiscussionRepository.java
@@ -0,0 +1,20 @@
+package edu.lawrence.data;
+
+import edu.lawrence.to.Discussion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Date;
+
+public interface DiscussionRepository extends JpaRepository<Discussion, Long> {
+	Discussion findById(String discussionid);
+	List<Discussion> findAll();
+	List<Discussion> findByTitleContaining(String title);
+	List<Discussion> findByCategoryAndTitleContaining(String category, String title);
+	List<Discussion> findByCategory(String category);
+	List<Discussion> findByDateGreaterThanEqual(Date date);
+	List<Discussion> findByCategoryAndDateGreaterThanEqual(String category, Date date);
+	List<Discussion> findByDateLessThanEqual(Date date);
+	List<Discussion> findByCategoryAndDateLessThanEqual(String category, Date date);
+	List<Discussion> findAllOrderByDateDesc();
+	List<Discussion> findAllOrderByCategoryDesc();
+}
