826e1a42bd3ce58a39afda5d0e5b20d1d24944bf
Malik Graham <malikgraham15@yahoo.com>
2016-04-02 15:34:04 -0500

    Added the Rating repository

---
+++ workspace/CoreAppServer/src/main/java/edu/lawrence/data/RatingRepository.java
@@ -0,0 +1,11 @@
+package edu.lawrence.data;
+
+import edu.lawrence.to.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author Malik Graham
+ */
+public interface RatingRepository {
+		
+}
